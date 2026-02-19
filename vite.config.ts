1c71b83190003c336d8c7dc88874ffbd5d1a6955
HabebeZyad <habebazyad2114@gmail.com>
2026-02-20 00:13:57 +0200

    Fix: switch to relative paths for assets to resolve white page

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/Comes-TO-life-/',
+    base: './',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
